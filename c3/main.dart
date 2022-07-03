fd1d9f019218eeeb03f80f9c029abb1f64f8d00e
yydcq123 <57498133+yydcq123@users.noreply.github.com>
2022-07-03 22:57:52 +0800

    Add files via upload

---
+++ c3/main.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              '测试字体',
+              style: TextStyle(fontSize: 30.0, fontFamily: "Raleway"),
+            ),
+            ElevatedButton(
+              child: const Text("normal"),
+              onPressed: () {},
+            ),
+            TextButton(
+              child: const Text("normal"),
+              onPressed: () {},
+            ),
+            IconButton(
+              icon: const Icon(Icons.thumb_up),
+              onPressed: () {},
+            ),
+            ElevatedButton.icon(
+              icon: const Icon(Icons.send),
+              label: const Text("发送"),
+              onPressed: () => {},
+            ),
+            const SwitchAndCheckBoxTestRoute(),
+            const FormTestRoute(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+class SwitchAndCheckBoxTestRoute extends StatefulWidget {
+  const SwitchAndCheckBoxTestRoute({Key? key}) : super(key: key);
+
+  @override
+  _SwitchAndCheckBoxTestRouteState createState() => _SwitchAndCheckBoxTestRouteState();
+}
+
+class _SwitchAndCheckBoxTestRouteState extends State<SwitchAndCheckBoxTestRoute> {
+  bool _switchSelected=true; //维护单选开关状态
+  bool _checkboxSelected=true;//维护复选框状态
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Switch(
+          value: _switchSelected,//当前状态
+          onChanged:(value){
+            setState(() {
+              _switchSelected=value;
+            });
+          },
+        ),
+        Checkbox(
+          value: _checkboxSelected,
+          activeColor: Colors.red, //选中时的颜色
+          onChanged:(value){
+            setState(() {
+              _checkboxSelected=value!;
+            });
+          } ,
+        )
+      ],
+    );
+  }
+}
+class FormTestRoute extends StatefulWidget {
+  const FormTestRoute({Key? key}) : super(key: key);
+
+  @override
+  _FormTestRouteState createState() => _FormTestRouteState();
+}
+
+class _FormTestRouteState extends State<FormTestRoute> {
+  final TextEditingController _unameController = TextEditingController();
+  final TextEditingController _pwdController = TextEditingController();
+  final GlobalKey _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey, //设置globalKey，用于后面获取FormState
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        children: <Widget>[
+          TextFormField(
+            autofocus: true,
+            controller: _unameController,
+            decoration: const InputDecoration(
+              labelText: "用户名",
+              hintText: "用户名或邮箱",
+              icon: Icon(Icons.person),
+            ),
+            // 校验用户名
+            validator: (v) {
+              return v!.trim().isNotEmpty ? null : "用户名不能为空";
+            },
+          ),
+          TextFormField(
+            controller: _pwdController,
+            decoration: const InputDecoration(
+              labelText: "密码",
+              hintText: "您的登录密码",
+              icon: Icon(Icons.lock),
+            ),
+            obscureText: true,
+            //校验密码
+            validator: (v) {
+              return v!.trim().length > 5 ? null : "密码不能少于6位";
+            },
+          ),
+          // 登录按钮
+          Padding(
+            padding: const EdgeInsets.only(top: 28.0),
+            child: Row(
+              children: <Widget>[
+                Expanded(
+                  child: ElevatedButton(
+                    child: const Padding(
+                      padding: EdgeInsets.all(16.0),
+                      child: Text("登录"),
+                    ),
+                    onPressed: () {
+                      // 通过_formKey.currentState 获取FormState后，
+                      // 调用validate()方法校验用户名密码是否合法，校验
+                      // 通过后再提交数据。
+                      if ((_formKey.currentState as FormState).validate()) {
+                        //验证通过提交数据
+                      }
+                    },
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
